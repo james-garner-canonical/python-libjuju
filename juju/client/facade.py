75646e8db2b8b7e9a4f9176f608b9e3ace6f6711
James Garner <james.garner@canonical.com>
2024-10-24 21:25:06 +0200

    style: split typing imports across multiple lines

--- juju/client/facade.py
+++ juju/client/facade.py
@@ -14,7 +14,17 @@ import typing
 from collections import defaultdict
 from glob import glob
 from pathlib import Path
-from typing import Any, Awaitable, Callable, Dict, List, Mapping, Protocol, Sequence, TypeVar
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    List,
+    Mapping,
+    Protocol,
+    Sequence,
+    TypeVar,
+)
 
 import typing_inspect
 from typing_extensions import ParamSpec
